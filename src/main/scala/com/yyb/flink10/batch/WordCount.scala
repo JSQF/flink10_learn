10ed5d846642cc8a8e607366caa07a7417757a12
jsqf <yangyingbo151006>
2020-04-15 17:40:24 +0800

    init commit

---
+++ src/main/scala/com/yyb/flink10/batch/WordCount.scala
@@ -0,0 +1,24 @@
+package com.yyb.flink10.batch
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+/**
+  * @Author yyb
+  * @Description
+  * @Date Create in 2020-04-15
+  * @Time 17:00
+  */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val filePtah = "/Users/yyb/Downloads/1.txt"
+    val text =  env.readTextFile(filePtah)
+    val wordCounts = text.flatMap(_.toLowerCase.split("\\W+") filter { _.nonEmpty})
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+    wordCounts.setParallelism(1).print()
+  }
+}
