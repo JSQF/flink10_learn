0710d79375f0b594351a01495e6bb9b0501c50c5
jsqf <yangyingbo151006>
2020-04-25 21:14:53 +0800

    operator parquet writer failed

---
+++ src/main/scala/com/yyb/flink10/table/flink/batch/BatchReadFromParquetQuery.scala
@@ -0,0 +1,11 @@
+package com.yyb.flink10.table.flink.batch
+
+/**
+  * @Author yyb
+  * @Description
+  * @Date Create in 2020-04-23
+  * @Time 17:40
+  */
+object BatchReadFromParquetQuery {
+
+}
---
+++ src/main/scala/com/yyb/flink10/table/flink/batch/BatchReadFromSequenceQuery.scala
@@ -0,0 +1,11 @@
+package com.yyb.flink10.table.flink.batch
+
+/**
+  * @Author yyb
+  * @Description
+  * @Date Create in 2020-04-24
+  * @Time 09:29
+  */
+object BatchReadFromSequenceQuery {
+
+}
