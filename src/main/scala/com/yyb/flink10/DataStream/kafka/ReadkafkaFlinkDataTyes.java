84db285a1aef571e6316a07e786253578a12cdab
jsqf <yangyingbo151006>
2020-09-14 09:43:19 +0800

    add bytes kafka read

--- src/main/scala/com/yyb/flink10/DataStream/kafka/ReadkafkaFlinkDataTyes.java
+++ src/main/scala/com/yyb/flink10/DataStream/kafka/ReadkafkaFlinkDataTyes.java
@@ -1,12 +1,18 @@
 package com.yyb.flink10.DataStream.kafka;
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Properties;
 
 /**
@@ -36,10 +42,26 @@ public class ReadkafkaFlinkDataTyes {
 
 
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", prop.getProperty("bootstrap.servers"));
-        properties.setProperty("zookeeper.connect", prop.getProperty("zookeeper.connect"));
+//        properties.setProperty("bootstrap.servers", prop.getProperty("bootstrap.servers"));
+        properties.setProperty("bootstrap.servers", "172.16.11.17:9092,172.16.11.18:9092,172.16.11.19:9092");
+//        properties.setProperty("zookeeper.connect", prop.getProperty("zookeeper.connect"));
+        properties.setProperty("zookeeper.connect", "njtest-cdh6-nn01.nj:2181,njtest-cdh6-nn02.nj:2181,njtest-cdh6-nn03.nj:2181");
         properties.setProperty("group.id", "test");
 
+
+        FlinkKafkaConsumer011<String> kafkaSource = new FlinkKafkaConsumer011<String>("as28-50p", new SimpleStringSchema(), properties);
+        DataStreamSource<String> source = env.addSource(kafkaSource);
+        source.map(new MapFunction<String, Object>() {
+            @Override
+            public Object map(String str) throws Exception {
+                System.out.println(Arrays.toString(str.getBytes()));
+                System.out.println(DatatypeConverter.printHexBinary(str.getBytes()));
+                System.out.println();
+                    return null;
+            }
+        });
+
+        env.execute("ReadkafkaFlinkDataTyes");
 
 
     }
